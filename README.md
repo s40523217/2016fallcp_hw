7fad7c16c2da5cb0d6da8e1d4c3b39d036e1eb61
s40523217 <40523217@gm.nfu.edu.tw>
2016-11-14 14:30:13 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# S40523217_hw
+# 2016fallcpa
+2016fall 計算機程式課程一甲評量倉儲
+
+https://mdecourse.github.io/2016fallcpa
+
+https://mdecourse.github.io/2016fallcpa/blog
+
--- pelicanconf.py
+++ pelicanconf.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = '40523217'
-SITENAME = '2016Fall CPB 課程網誌 (虎尾科大MDE)'
+AUTHOR = 's40523217'
+SITENAME = '2016Fall 課程網誌 (虎尾科大MDE)'
 # 不要用文章所在目錄作為類別
 USE_FOLDER_AS_CATEGORY = False
 
@@ -38,7 +38,7 @@ DEFAULT_PAGINATION = 10
 
 # 必須絕對目錄或相對於設定檔案所在目錄
 PLUGIN_PATHS = ['plugin']
-PLUGINS = ['liquid_tags.notebook', 'summary', 'tipue_search', 'sitemap']
+PLUGINS = ['liquid_tags', 'summary', 'tipue_search', 'sitemap']
 
 # for sitemap plugin
 SITEMAP = {
